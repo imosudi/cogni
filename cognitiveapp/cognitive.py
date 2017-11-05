b6d032bcd83515ca546b38df10897caff4cb012d
imosudi <imosudi@gmail.com>
2017-11-05 06:55:29 +0100

    matplotlib

--- cognitiveapp/cognitive.py
+++ cognitiveapp/cognitive.py
@@ -12,7 +12,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
-
+import time
 import io
 import base64
 import matplotlib
@@ -45,7 +45,7 @@ def index():
     t1 = np.arange(0.0, 5.0, 0.1)
     t2 = np.arange(0.0, 5.0, 0.02)
     plt.figure(1)
-    plt.subplot(211)
+    plt.subplot(111)
     ft = np.exp(-t1) * np.cos(2*np.pi*t1)
     ftt = np.exp(-t2) * np.cos(2*np.pi*t2)
     plt.plot(t1, ft, 'bo', t2, ftt, 'k') 
@@ -56,7 +56,32 @@ def index():
 
     return render_template("index.html", plot_url=plot_url)
 
+@app.route('/live')
+def live_update_demo(blit = False):
+    img = io.BytesIO()
+    x = np.linspace(0,75., num=200)
+    plt.figure()
+    plt.subplot(1, 1, 1)
 
+    h1, = plt.plot(x, lw=3)
+    plt.text(0.8,1.5, "")
+    plt.ylim([-1,1])
+
+
+    t_start = time.time()
+    k=0.
+    for i in np.arange(1000):
+        h1.set_ydata(np.sin(x/3.+k))
+        k+=0.71 #wave frequency
+ 
+        plt.pause(0.000000000001)
+        plt.savefig(img, format='png', dpi=965)
+	img.seek(0)
+	plot_url = base64.b64encode(img.getvalue())#.decode()
+	return render_template("index.html", plot_url=plot_url)
+ 
+
+"""live_update_demo(True)"""
 
 if __name__ == '__main__':
     #manager.run(host='0.0.0.0')
